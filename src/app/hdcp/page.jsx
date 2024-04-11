7b95cb453ba06a7a7d7531c934042997d20ae4bf
chris dziedzic <cdziedzic@gmail.com>
2024-04-11 15:27:23 -0400

    setup basic routing

---
+++ src/app/hdcp/page.jsx
@@ -0,0 +1,10 @@
+export default function HdcpPage() {
+  return (
+    <main>
+      <h1>My Handicap</h1>
+      <p>
+        This is where you will be able to track your handicap.
+      </p>
+    </main>
+  );
+}
