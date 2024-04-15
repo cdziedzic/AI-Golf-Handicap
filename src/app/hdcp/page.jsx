c77594554f98ba78b90e8f8193b3b93f0fdc2ece
chris dziedzic <cdziedzic@gmail.com>
2024-04-15 14:20:21 -0400

    switched to assistant instead of chatbot

--- src/app/hdcp/page.jsx
+++ src/app/hdcp/page.jsx
@@ -1,24 +1,52 @@
-'use client';
- 
-import { useChat } from 'ai/react';
- 
+'use client'
+import { useAssistant } from 'ai/react';
+
+const roleToColorMap = {
+  system: 'red',
+  user: 'black',
+  function: 'blue',
+  assistant: 'green',
+  data: 'orange',
+};
+
 export default function Chat() {
-  const { messages, input, handleInputChange, handleSubmit } = useChat();
-  
+  const { status, messages, input, submitMessage, handleInputChange } = useAssistant({ api: '/api/chat' });
+
   return (
     <div className="flex flex-col w-full max-w-md py-24 mx-auto stretch">
-      {messages.map(m => (
-        <div key={m.id} className="whitespace-pre-wrap">
-          {m.role === 'user' ? 'User: ' : 'AI: '}
-          {m.content}
+      {messages.map((m) => (
+        <div
+          key={m.id}
+          className="whitespace-pre-wrap"
+          style={{ color: roleToColorMap[m.role] }}
+        >
+          <strong>{`${m.role}: `}</strong>
+          {m.role !== 'data' && m.content}
+          {m.role === 'data' && (
+            <>
+              {/* here you would provide a custom display for your app-specific data:*/}
+              {m.data.description}
+              <br />
+              <pre className={'bg-gray-200'}>
+                {JSON.stringify(m.data, null, 2)}
+              </pre>
+            </>
+          )}
+          <br />
+          <br />
         </div>
       ))}
- 
-      <form onSubmit={handleSubmit}>
+
+      {status === 'in_progress' && (
+        <div className="h-8 w-full max-w-md p-2 mb-8 bg-gray-300 dark:bg-gray-600 rounded-lg animate-pulse" />
+      )}
+
+      <form onSubmit={submitMessage}>
         <input
+          disabled={status !== 'awaiting_message'}
           className="fixed bottom-0 w-full max-w-md p-2 mb-8 border border-gray-300 rounded shadow-xl"
           value={input}
-          placeholder="Say something..."
+          placeholder="What is the temperature in the living room?"
           onChange={handleInputChange}
         />
       </form>
