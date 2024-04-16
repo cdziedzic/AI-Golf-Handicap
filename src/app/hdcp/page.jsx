84d18599f09293f7b28ad52cc6d5d8c84bf2b649
chris dziedzic <cdziedzic@gmail.com>
2024-04-15 20:31:45 -0400

    updated function for tavily

--- src/app/hdcp/page.jsx
+++ src/app/hdcp/page.jsx
@@ -46,7 +46,7 @@ export default function Chat() {
           disabled={status !== 'awaiting_message'}
           className="fixed bottom-0 w-full max-w-md p-2 mb-8 border border-gray-300 rounded shadow-xl"
           value={input}
-          placeholder="What is the temperature in the living room?"
+          placeholder="Tell us about your round?"
           onChange={handleInputChange}
         />
       </form>
